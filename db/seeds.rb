26b18e67bcec7364beb931eeef019a7cc1791576
Julie <julie.lin0812@gmail.com>
2017-10-08 00:38:37 -0700

    Adds additional seeds to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 Product.destroy_all
+Order.destroy_all
+OrderedItem.destroy_all
 
 Product.create!(name: 'Cafe Americano', price: 2.00, description: 'great pick me up!', category: 'drink', inventory: rand(10))
 Product.create!(name: 'Red Bull', price: '3.00', description: 'get energized!', category: 'drink', inventory: rand(10))
 Product.create!(name: 'Doritos Nacho', price: '1.00', description: 'hungry?', category: 'food', inventory: rand(10))
 Product.create!(name: 'Cup Ramen', price: '1.50', description: 'it\'s more than just soup!', category: 'food', inventory: rand(10))
 Product.create!(name: 'Mints', price: '4.00', description: 'Good for pair programming', category: 'food', inventory: rand(10))
+
+User.create!(email: "randomemail@email.com", stripe_token: "1")
+
+Order.create!(customer_id: 1, status: "ordered")
+
+OrderedItem.create!(order_id: 1, product_id: 1)
+OrderedItem.create!(order_id: 1, product_id: 2)
+OrderedItem.create!(order_id: 1, product_id: 3)
