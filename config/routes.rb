ef734c291d2ba86c3b244223894a6e3d81b963c3
Julie <julie.lin0812@gmail.com>
2017-10-10 20:15:31 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: :create
+    resources :users, only: :create do
+      collection do
+        # post 'confirm'
+        post 'login'
+      end
+    end
     resources :products, only: %i(index create new edit update destroy)
     resources :orders, only: %i(index show create update)
     resources :ordered_items, only: :create
