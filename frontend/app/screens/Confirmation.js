f5cde77498ec44357c22ea1963b3dd36e74af053
Julie <julie.lin0812@gmail.com>
2017-10-11 14:49:46 -0700

    Change styling on botton nav buttons

--- frontend/app/screens/Confirmation.js
+++ frontend/app/screens/Confirmation.js
@@ -21,13 +21,15 @@ class Confirmation extends React.Component {
     return (
       <View style={{flex: 1}}>
         <HeaderBanner style={{flex: 1}}/>
-        <View style={{flex: .5, backgroundColor: '#f7f7f7', justifyContent: 'center'}}>
+        <View style={{flex: 1, backgroundColor: '#f7f7f7', justifyContent: 'center'}}>
           <Text style={{alignSelf: 'center', color: 'white'}}>Order Confirmed</Text>
         </View>
-        <Image style={{flex: 5, width: 400}} source={orderConfirmed}/>
-        <Container style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+        <Image style={{flex: 7, width: 400}} source={orderConfirmed}/>
+        <Container style={
+          {flex: 1, flexDirection: 'row', justifyContent: 'space-around',
+          alignItems: 'center', backgroundColor: '#1485CC'}}>
           <TouchableOpacity onPress={() => navigate('Index')}>
-            <Text style={{fontSize: 22}}>Back to Menu</Text>
+            <Text style={{color: '#FFFFFF', fontSize: 18}}>Back to Menu</Text>
           </TouchableOpacity>
         </Container>
       </View>
