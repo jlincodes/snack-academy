b95fa569b647ee1051a4f762bdcf191931f78f1f
Julie <julie.lin0812@gmail.com>
2017-10-11 11:52:34 -0700

    Change styling on botton nav buttons

--- frontend/app/screens/Cart.js
+++ frontend/app/screens/Cart.js
@@ -20,18 +20,20 @@ class Cart extends React.Component {
     const { goBack, navigate } = this.props.navigation;
     let exampleCart = require('../images/cart.png');
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
         <HeaderBanner style={{flex: 1}}/>
-        <View style={{flex: .5, justifyContent: 'center'}}>
+        <View style={{flex: 1, justifyContent: 'center'}}>
           <Text style={{alignSelf: 'center', color: 'black'}}>Your Cart</Text>
         </View>
-        <Image style={{flex: 5, width: 400}} source={exampleCart}/>
-        <Container style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+        <Image style={{flex: 7, width: 400}} source={exampleCart}/>
+        <Container style={
+          {flex: 1, flexDirection: 'row', justifyContent: 'space-around',
+          alignItems: 'center', backgroundColor: '#1485CC'}}>
           <TouchableOpacity onPress={() => navigate('Index')}>
-            <Text style={{fontSize: 22}}>Back to Menu</Text>
+            <Text style={{color: '#FFFFFF', fontSize: 18}}>Back to Menu</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => navigate('CheckOut')}>
-            <Text style={{fontSize: 22}}>Check Out</Text>
+            <Text style={{color: '#FFFFFF', fontSize: 18}}>Check Out</Text>
           </TouchableOpacity>
         </Container>
       </View>
