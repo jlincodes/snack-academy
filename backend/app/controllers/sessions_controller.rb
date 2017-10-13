458503bb4fbf3cb36cf77093ad0555cdd232f631
Julie <julie.lin0812@gmail.com>
2017-10-13 00:11:58 -0700

    Add session controller

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @admin = Admin.find_by_credentials(
+      params[:admin][:username],
+      params[:admin][:password]
+    )
+    if @admin
+      login(@admin)
+      redirect_to 'api/orders/index'
+    else
+      flash[:errors] = ['Invalid Login Credentials']
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+end
