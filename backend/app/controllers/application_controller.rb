7bdedc1ee85bf2a1048d0b7ddeefd3a63cb2ed21
Julie <julie.lin0812@gmail.com>
2017-10-13 11:45:50 -0700

    Update application controller for admin login auth

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,7 +7,26 @@ class ApplicationController < ActionController::Base
     @current_user
   end
 
+  # for dashboard login
+  def current_admin
+    @current_admin = Admin.find_by_session_token(session[:session_token])
+  end
+
   def logged_in?
     !!current_admin
+  end
+
+  def login(admin)
+    @current_admin = admin
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
   end
 end
