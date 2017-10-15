be64223b257026ae5d0872b0ca9d091c1d997fcb
Julie <julie.lin0812@gmail.com>
2017-10-14 20:08:36 -0700

    Change product price column type to integer

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013044233) do
+ActiveRecord::Schema.define(version: 20171015025405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20171013044233) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
-    t.float "price", null: false
+    t.integer "price", null: false
     t.string "description", null: false
     t.string "category", null: false
     t.integer "inventory", null: false
