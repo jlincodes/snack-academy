92138f7c59e9c1c6219850eaeafb2f24fbf8f274
Julie <julie.lin0812@gmail.com>
2017-10-15 13:14:57 -0700

    Assign default value to order status column

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015034258) do
+ActiveRecord::Schema.define(version: 20171015201136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20171015034258) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "status", null: false
+    t.string "status", default: "ordered", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
