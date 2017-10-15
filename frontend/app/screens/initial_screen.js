f43fb9488d822e31e9cc3ab0321a3ab65512bd55
Logan Cooper <ldtcoop@gmail.com>
2017-10-15 16:20:02 -0700

    Add async store get

--- frontend/app/screens/initial_screen.js
+++ frontend/app/screens/initial_screen.js
@@ -1,21 +1,23 @@
 import React from 'react';
-
-function isLoggedIn() {
-
-  let loggedIn = false;
-
-  if(loggedIn) {
-    return true;
-  } else {
-    return false;
-  }
-}
+import { ActivityIndicator, AsyncStorage } from 'react-native';
 
 class InitialScreen extends React.Component {
 
-  componentWillMount(){
+  constructor() {
+    super();
+    this.state = {hasToken: false, isLoaded: false};
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('@snackOverflowAuthKey:key').then( (token) => {
+      this.setState({hasToken: token !== null, isLoaded: true});
+    });
+  }
+
+  componentWillUpdate(){
     const { navigate } = this.props.navigate;
-    if (isLoggedIn) {
+    if (this.state.hasToken) {
+      // send token to backend to get user info
       navigate('Index');
     } else {
       navigate('Signup');
@@ -23,7 +25,9 @@ class InitialScreen extends React.Component {
   }
 
   render(){
-    return null;
+    return(
+      <ActivityIndicator />
+    );
   }
 
 }
