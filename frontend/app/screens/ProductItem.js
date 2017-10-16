7f7de2557cb4f5c297d0b5be5b2e419f143b59c1
Julie <julie.lin0812@gmail.com>
2017-10-16 00:22:59 -0700

    Add styling

--- frontend/app/screens/ProductItem.js
+++ frontend/app/screens/ProductItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { StyleSheet, TextInput, TouchableOpacity, Text, AppRegistry, View, Button, Image } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  AppRegistry,
+  View,
+  Button,
+  Image
+} from 'react-native';
 import {connect} from 'react-redux';
 
-import { addItemToCart } from '../actions/cart_actions.js'
+import { addItemToCart } from '../actions/cart_actions.js';
 
 class ProductItem extends React.Component {
   constructor(props) {
@@ -15,10 +24,6 @@ class ProductItem extends React.Component {
     return dollars.toLocaleString("en-US", {style:"currency", currency:"USD"});
   }
 
-  // <Image style={{height: 150, width: '100%' }} source={{uri: product.img_url}} />
-  // <Text>{product.name}</Text>
-  // <Text>{product.description}</Text>
-  // <Text>{this.handleUSDConversion(product.price)}</Text>
   render() {
     let product = this.props.product;
     let name = product.name;
@@ -26,21 +31,42 @@ class ProductItem extends React.Component {
     let price = this.handleUSDConversion(product.price);
     return (
       <View>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', margin: 10}}>
+        <View style={styles.menuItem}>
           <Image
-            style={{height: 125, width: 125, marginRight: 15 }}
+            style={styles.image}
             source={{uri: product.img_url}} />
-          <Text>
+          <Text style={styles.text}>
             {`${name}\n${description}\n${price}`}
           </Text>
         </View>
 
-        <Button title='add item to cart' onPress={() => this.props.addItemToCart(product)}/>
+        <Button
+          title='add item to cart'
+          onPress={() => this.props.addItemToCart(product)}/>
       </View>
     );
   }
+}
 
-  }
+  const styles = StyleSheet.create({
+    menuItem: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      margin: 10
+    },
+    image: {
+      height: 100,
+      width: 100,
+      marginRight: 30,
+      marginLeft: 15
+    },
+    text: {
+      fontSize: 16,
+      color: '#555555'
+    }
+  });
+
 
   //testing
   const mapStateToProps = (state) => ({
@@ -54,7 +80,7 @@ class ProductItem extends React.Component {
       {
         addItemToCart: (item) => dispatch(addItemToCart(item))
       }
-    )
+    );
   };
 
   export default connect(mapStateToProps, mapDispatchToProps)(ProductItem);
