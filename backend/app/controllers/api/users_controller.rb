1cb9b9dcfc3875873965ca5f594e4e74593e48e8
Logan Cooper <ldtcoop@gmail.com>
2017-10-15 20:07:59 -0700

    Change user find:

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -4,10 +4,11 @@ class Api::UsersController < ApplicationController
   end
 
   def verify
-    @user = User.find_by(:auth_key, params[:user][:auth_key])
+    @user = User.find_by(auth_key: params[:user][:auth_key])
     if @user
       render :show
     else
+      puts "ERROR HERE: #{@user.errors.full_messages}"
       render json: "No such user found!", status: 404
     end
   end
