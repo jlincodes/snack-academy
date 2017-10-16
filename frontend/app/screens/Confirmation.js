cf88a3ff88e270c05f42e0ea002b590ea4e34206
Julie <julie.lin0812@gmail.com>
2017-10-16 01:10:40 -0700

    Add changes

--- frontend/app/screens/Confirmation.js
+++ frontend/app/screens/Confirmation.js
@@ -13,25 +13,29 @@ import { Container } from 'native-base';
 import { connect } from 'react-redux';
 
 import HeaderBanner from './HeaderBanner.js';
-import { clearCart } from '../actions/cart_actions.js'
+import { clearCart } from '../actions/cart_actions.js';
 
 class Confirmation extends React.Component {
 
   componentWillMount(){
-    this.props.clearCart()
+    this.props.clearCart();
   }
 
+  // let orderConfirmed = require('../images/order_confirmed.png');
+  // <Image style={{flex: 7, width: 400}} source={orderConfirmed}/>
 
   render() {
     const { goBack, navigate } = this.props.navigation;
-    let orderConfirmed = require('../images/order_confirmed.png');
+    let confirmation = this.props.confirmation;
     return (
       <View style={{flex: 1}}>
         <HeaderBanner style={{flex: 1}}/>
-        <View style={{flex: 1, backgroundColor: '#f7f7f7', justifyContent: 'center'}}>
-          <Text style={{alignSelf: 'center', color: 'white'}}>Order Confirmed</Text>
+        <View style={{flex: 10, backgroundColor: '#f7f7f7', flexDirection: 'column', justifyContent: 'center'}}>
+          <Text style={styles.text}>Your order has been confirmed.</Text>
+          <Text style={styles.text}>{`Your order number is: #${confirmation}`}</Text>
+          <Text style={styles.text}>{`\nPick Up Instructions:\nYour order will be ready for pick up during your break at 3:45 PM.`}</Text>
         </View>
-        <Image style={{flex: 7, width: 400}} source={orderConfirmed}/>
+
         <Container style={
           {flex: 1, flexDirection: 'row', justifyContent: 'space-around',
           alignItems: 'center', backgroundColor: '#1485CC'}}>
@@ -44,10 +48,19 @@ class Confirmation extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    color: '#444444'
+  }
+});
 
+const mapStateToProps = (state) => ({
+  confirmation: state.confirmation
+});
 
 const mapDispatchToProps = (dispatch) => ({
   clearCart: () => dispatch(clearCart())
 });
 
-export default connect(null, mapDispatchToProps)(Confirmation);
+export default connect(mapStateToProps, mapDispatchToProps)(Confirmation);
