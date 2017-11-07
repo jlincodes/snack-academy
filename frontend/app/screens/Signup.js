202ff90a668e72689d881a014fcd27580418c4c2
Kenta Kodama <kentakodama@gmail.com>
2017-11-07 01:34:43 -0800

    know that json can return user info

--- frontend/app/screens/Signup.js
+++ frontend/app/screens/Signup.js
@@ -16,7 +16,7 @@ import {
   Image
 } from 'react-native';
 import HeaderBanner from './HeaderBanner.js';
-import {initializeUser} from '../actions/user_actions.js';
+// import {initializeUser} from '../actions/user_actions.js';
 
 
 
@@ -25,9 +25,43 @@ import {
   AccessToken
 } from 'react-native-fbsdk';
 
+import * as firebase from 'firebase';
+
 class Signup extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {}
+    this.initializeUser = this.initializeUser.bind(this)
+  }
+
+  initializeUser(token) {
+    let user = {}
+    fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      // // Some user object has been set up somewhere, build that user here
+      // // console.log(json);
+      // this.setState({name: json.name})
+      this.setState({id: json.id})
+      this.setState({email: json.email})
+      // // user.user_friends = json.friends
+      user.email = json.email
+      // // user.username = json.name
+      // // user.loading = false
+      // // user.loggedIn = true
+      // user.avatar = setAvatar(json.id)
+
+    })
+    .catch(() => {
+      reject('ERROR GETTING DATA FROM FACEBOOK')
+    })
+    console.log(this.state);
+  }
+
   render() {
+
     return (
       <View>
         <LoginButton
@@ -42,8 +76,13 @@ class Signup extends React.Component {
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
                     console.log(data);
-                    console.log(result);
-                    alert(data.accessToken.toString())
+
+                    const token = data.accessToken;
+                    let user2 = this.initializeUser
+
+                    console.log('below');
+                    console.log(this.state);
+
                   }
                 )
               }
