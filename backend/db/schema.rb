c8be5475ad40c65a069a00e9afe1dfa6a969d6b4
Julie <julie.lin0812@gmail.com>
2017-11-08 15:48:22 -0800

    Migrate change to fbId column and update schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107192418) do
+ActiveRecord::Schema.define(version: 20171108204250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20171107192418) do
     t.string "customer_id"
     t.string "name"
     t.string "auth_key"
-    t.integer "fbId"
+    t.string "fbId"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["stripe_token"], name: "index_users_on_stripe_token", unique: true
   end
