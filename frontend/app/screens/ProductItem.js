f6298e7436abdb365461825a93285faf50bdd593
Kenta Kodama <kentakodama@gmail.com>
2017-11-13 13:16:33 -0800

    changed border for product item

--- frontend/app/screens/ProductItem.js
+++ frontend/app/screens/ProductItem.js
@@ -29,7 +29,7 @@ class ProductItem extends React.Component {
     let description = product.description;
     let price = this.handleUSDConversion(product.price);
     return (
-      <View style={{ borderWidth: 2, borderColor: '#d6d7da'}}>
+      <View style={{ borderWidth: 3, borderColor: '#283954'}}>
         <View style={styles.menuItem}>
           <Image
             style={styles.image}
@@ -61,6 +61,7 @@ class ProductItem extends React.Component {
     },
     text: {
       fontSize: 16,
+      fontWeight: 'bold',
       color: '#555555'
     }
   });
