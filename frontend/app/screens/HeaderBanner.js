791e1e027c21cb0c9e1aad8afb73b37771e6bd32
Kenta Kodama <kentakodama@gmail.com>
2017-11-12 23:04:18 -0800

    readjusted header banner logo

--- frontend/app/screens/HeaderBanner.js
+++ frontend/app/screens/HeaderBanner.js
@@ -5,8 +5,8 @@ import { Header, Title} from 'native-base';
 class HeaderBanner extends React.Component {
   render() {
     return (
-      <View style={{flex: 1.2, backgroundColor: '#C00A0A', justifyContent: 'center', alignItems: 'center'}}>
-        <Image style={{width: '70%', height: '85%', marginTop: 5}} resizeMode="cover" source={{uri: 'https://res.cloudinary.com/dql6mlrow/image/upload/v1510548598/SnackAcademyLogo.png'}} />
+      <View style={{flex: 1.3, backgroundColor: '#C00A0A', justifyContent: 'center', alignItems: 'center'}}>
+        <Image style={{width: '70%', height: '85%', marginTop: 10}} resizeMode="cover" source={{uri: 'https://res.cloudinary.com/dql6mlrow/image/upload/v1510548598/SnackAcademyLogo.png'}} />
       </View>
     );
   }
